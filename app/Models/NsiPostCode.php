b8616528ecd9840adbdd4da0c785c67eafbe9b91
Davydenko <davydenko.v@sunkt.ru>
2021-06-06 19:08:22 +0400

    models

---
+++ app/Models/NsiPostCode.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class NsiPostCode extends Model
+{
+    use HasFactory;
+}
